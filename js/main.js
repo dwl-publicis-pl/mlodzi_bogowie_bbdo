3454ea8fe58032940bdd4b52ba3ecec295751e28
czachor <czachorowski@bbdo.com.pl>
2015-09-30 16:06:25 +0200

    kalkulator 1 - miesięczne oszczędności cz. 2

--- js/main.js
+++ js/main.js
@@ -17,12 +17,23 @@ $(function() {
  * kalkulator 1
  */
 var monthlySavingsCalculator = (function() {
+    var retirementAge = 67;
     var calcForm = '#mscCalc';
     var $calcElem = $(calcForm);
-    var retirementLength = 20,
-        savings = 1500,
-        startSaveUp = 20,
-        monthlySaveUp = 0;
+    var retirementLengthElement = $('input[name="retirement_length"]');
+    var monthlySavingsElements = $('input[name="monthly_savings"]');
+    var startSaveUpElement = $('input[name="start_save_up"]');
+    var monthlySavingsElement = $('#retirement_saves_result');
+    
+    var retirementLengthValueElement = $('.retirement_length_value');
+    var monthlySavingsValueElements = $('.monthly_savings_value');
+    var startSaveUpValueElement = $('.start_save_up_value');
+    
+    var retirementLengthVal,
+        monthlySavingsVal,
+        startSaveUpVal,
+        saveUpLength;
+    var monthlySaveUp = 0;
 
     var init = function() {
         if ($calcElem.length > 0) {
@@ -33,21 +44,59 @@ var monthlySavingsCalculator = (function() {
     };
     
     var initListeners = function() {
-        var retirementLengthValue = $('input[name="retirementLength"]');
-        /*var range = $('.input-range'),
-            value = $('.range-value');
-            
-        value.html(range.attr('value'));
+        $(calcForm + ' input[type="range"]').on('input', function() {
+            onRangeChange();
+        });
         
-        range.on('input', function(){
-            value.html(this.value);
-        });*/ 
+        return true;
     };
+    
+    var onRangeChange = function() {
+        getValues();
+        showValues();
+        calculateMonthlySavings();
+        
+        return true;
+    }
+    
+    var getValues = function() {
+        retirementLengthVal = parseInt(retirementLengthElement.val());
+        monthlySavingsVal = parseInt(monthlySavingsElements.val());
+        startSaveUpVal = parseInt(startSaveUpElement.val());
+        saveUpLength = retirementAge - startSaveUpVal;
+        
+        return true;
+    }
+    
+    var showValues = function() {
+        retirementLengthValueElement.html(retirementLengthVal);
+        monthlySavingsValueElements.html(monthlySavingsVal);
+        startSaveUpValueElement.html(startSaveUpVal);
+        
+        return true;
+    }
+    
+    var calculateMonthlySavings = function() {
+        //console.log(retirementLengthVal, monthlySavingsVal, startSaveUpVal);
+        monthlySaveUp = ((retirementLengthVal * monthlySavingsVal * 12) / (saveUpLength * 12)).toFixed(2);
+        
+        proceedMonthlySavings();
+        
+        return true;
+    }
+    
+    var proceedMonthlySavings = function() {
+        monthlySavingsElement.html(monthlySaveUp);
+        
+        return true;
+    }
 
     return {
         init: init
     };
 })();
+
+monthlySavingsCalculator.init();
 /**
  * kalkulator 1 END
  */
--- kalkulator.php
+++ kalkulator.php
@@ -11,10 +11,26 @@ Tpl::htmlHead();
             
             <form role="form" id="mscCalc" action="#" method="post">
                 <fieldset>
-                    <div class="form-group range-slider">
+                    <div class="form-group">
                         <label id="f-msc-rl">Ile lat chcesz żyć na emeryturze?</label>
-                        <input name="retirement_length" type="range" min="10" max="30" step="5" class="input-range" id="f-msc-rl" value="20" autocomplete="off">
-                        <span class="range-value">20</span>
+                        <input name="retirement_length" type="range" min="10" max="30" step="5" class="form-control" id="f-msc-rl" value="20">
+                        <span class="retirement_length_value">20</span>
+                    </div>
+                    
+                    <div class="form-group">
+                        <label id="f-msc-ms">Ile dodatkowo chcesz dostawać miesięcznie PLN na emeryturze?</label>
+                        <input name="monthly_savings" type="range" min="500" max="5000" step="500" class="form-control" id="f-msc-ms" value="1500">
+                        <span class="monthly_savings_value">1500</span>
+                    </div>
+                    
+                    <div class="form-group">
+                        <label id="f-msc-ss">Kiedy chcesz zacząć oszczędać?</label>
+                        <input name="start_save_up" type="range" min="20" max="66" step="1" class="form-control" id="f-msc-ss" value="20">
+                        <span class="start_save_up_value">20</span>
+                    </div>
+                    
+                    <div>
+                        Oszczędzaj miesięcznie <span id="retirement_saves_result">...</span> zł.
                     </div>
                 </fieldset>
             </form>
