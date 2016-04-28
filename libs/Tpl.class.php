b01d017be5bc894cf91ff1eb23d079b20b0d1cdc
czachor <czachor@users.noreply.github.com>
2016-04-28 11:01:35 +0200

    FB pixel

--- libs/Tpl.class.php
+++ libs/Tpl.class.php
@@ -54,6 +54,19 @@ class Tpl {
         </head>
 
         <body<?php echo $bodyClass; ?>>
+            <!-- Facebook Pixel Code -->
+            <script>
+                !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+                    n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+                    t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+                    document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+                fbq('init', '947773291996561');
+                fbq('track', "PageView");</script>
+            <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=947773291996561&ev=PageView&noscript=1" /></noscript>
+            <!-- End Facebook Pixel Code -->
+
             <!-- Google Tag Manager -->
             <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-TH6V2V" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
             <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
